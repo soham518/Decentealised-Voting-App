1edb11fed40053af607f0a56a20b6557472b6b08
soham518 <167617833+soham518@users.noreply.github.com>
2025-09-29 17:34:47 +0530

    set up web3 context and provider

---
+++ src/Dummy.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Dummy = () => {
+  return (
+
+    <>
+        <div>Dummy component</div>
+    </>
+  )
+}
+
+export default Dummy
---
+++ src/context/Web3Provider.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { Web3Context } from "./web3Context";
+
+const Web3Provider = () => {
+  const [web3State, setWeb3State] = useState({
+    contractInstance: null,
+    selectedAccount: null,
+    chainId: null,
+  });
+
+  return (
+    <>
+      <Web3Context.Provider value={web3State}>{children}</Web3Context.Provider>
+      <button>Connect to Wallet</button>
+    </>
+  );
+};
+
+export default Web3Provider;
