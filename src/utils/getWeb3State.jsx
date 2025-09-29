96afdf56cd6284503d481dcceca556234e257f70
soham518 <167617833+soham518@users.noreply.github.com>
2025-09-29 18:17:50 +0530

    create signer and contract instance.

--- src/utils/getWeb3State.jsx
+++ src/utils/getWeb3State.jsx
@@ -1,3 +1,4 @@
+import abi from "../constant/abi.json"
 export const getWeb3State = async () => {
   //check if metamask wallet exesist
   try {
@@ -5,9 +6,20 @@ export const getWeb3State = async () => {
       throw new Error("Meatmask is not installed");
     }
 
-    const accounts = window.ethereum.requests({
+    const accounts = window.ethereum.request({
       "method": "eth_requestAccounts",
     });
+
+    const selectedAccount = accounts[0];
+    const chainIdHex = window.ethereum.request({
+        "method":"eth_chainId"
+    })
+    const chainId = parseInt(chainIdHex,16);
+    //getting provider and signer in order to access contract instance.
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner();
+    const contractAddress = "";
+    const contractInstance = new ethers.Contract(contractAddress,abi,signer);
   } catch (error) {
     console.error(error);
   }
