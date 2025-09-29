37d70927d2a06039d86ca0d2c455bbf6ede4ef89
soham518 <167617833+soham518@users.noreply.github.com>
2025-09-29 23:03:52 +0530

    add handle account change and chain id change in utility functions

---
+++ src/utils/handleChainChange.jsx
@@ -0,0 +1,7 @@
+export const hancleChainChange = async (setWeb3State) => {
+  const chainIdHex = await window.ethereum.request({
+    method: "eth_ChainId",
+  });
+  const chainId = parseInt(chainIdHex, 16);
+  setWeb3State((prevState) => ({ ...prevState, chainId }));
+};
