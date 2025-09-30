e376d4f402e1e8ee51d8d23e9099b667e78625f2
soham518 <167617833+soham518@users.noreply.github.com>
2025-09-30 09:20:53 +0530

    minor bug fix

---
+++ src/pages/Candidate/RegisterCandidate.jsx
@@ -0,0 +1,54 @@
+import { useRef } from "react";
+import { useWeb3Context } from "../../context/useWeb3Context";
+const RegisterVoter = () => {
+  const {contractInstance} = useWeb3Context();
+  const nameRef = useRef(null);
+  const ageRef = useRef(null);
+  const genderRef = useRef(null);
+  const partyRef = useRef(null);
+  const handleCandidateRegestration = async(e) => {
+    try {
+      e.preventDefault();
+      const name = nameRef.current.value;
+      const age = ageRef.current.value;
+      const gender = genderRef.current.value;
+      const party = partyRef.current.value;
+
+      await contractInstance.registerCandidate(name,party,age,gender);
+
+      console.log("candidate registered successfully");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <h2>Register Voter</h2>
+      <form onSubmit={handleCandidateRegestration}>
+        <label>
+          Name: <input type="text" ref={nameRef}></input>
+        </label>
+
+        <label>
+          Age: <input type="number" ref={ageRef}></input>
+        </label>
+
+        <label>
+          Gender:{" "}
+          <input type="text" ref={genderRef}>
+            {" "}
+          </input>
+        </label>
+
+        <label>
+          Party: <input type="text" ref={partyRef}></input>
+        </label>
+
+        <button type="submit">Register</button>
+      </form>
+    </>
+  );
+};
+
+export default RegisterVoter;
