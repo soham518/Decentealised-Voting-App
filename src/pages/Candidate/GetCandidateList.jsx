14505e0f3c9189649c821b5adc3ea940a0f5969a
soham518 <167617833+soham518@users.noreply.github.com>
2025-10-01 13:55:50 +0530

    add GetCandidateList component

---
+++ src/pages/Candidate/GetCandidateList.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { useWeb3Context } from "../../context/useWeb3Context";
+const GetCandidateList = () => {
+  const { contractInstance } = useWeb3Context();
+  const [candidateList, setCandidateList] = useState([]);
+  useEffect(() => {
+    const fetchCandidateList = async () => {
+      try {
+        if (contractInstance) {
+          const candidates = await contractInstance.getCandidateList();
+          setCandidateList(candidates);
+          console.log(candidates);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    contractInstance && fetchCandidateList();
+  }, [contractInstance]);
+  return (
+    <>
+      <h2>Candidate List</h2>
+
+      {candidateList.map((candidates, index) => {
+        <li key={index}> {candidates}</li>
+      })}
+    </>
+  );
+};
+
+export default GetCandidateList;
